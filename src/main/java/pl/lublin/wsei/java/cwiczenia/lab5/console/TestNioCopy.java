a09307e801e3cc3e85a698a031c013a629fe8fbc
Lukmajcher <100759817+LMajcherWsei@users.noreply.github.com>
2022-06-17 20:46:00 +0200

    Punkt 9. Dodano alternatywny kod z wykorzystaniem File zamiast Path

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/console/TestNioCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab5/console/TestNioCopy.java
@@ -11,9 +11,12 @@ public class TestNioCopy {
     public static void main(String[] args) throws IOException {
         //long startTime = System.currentTimeMillis();
         long startTime = System.nanoTime();
-        Path oryginal = Paths.get("sample_1920×1280.tiff");
-        Path kopia = Paths.get("img_copy2.tiff");
-        Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
+//        Path oryginal = Paths.get("sample_1920×1280.tiff");
+//        Path kopia = Paths.get("img_copy2.tiff");
+//        Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
+        File oryginal = new File("sample_1920×1280.tiff");
+        File kopia = new File("img_copy2.tiff");
+        Files.copy(oryginal.toPath(), kopia.toPath(),StandardCopyOption.REPLACE_EXISTING);
 
 
         //System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime) + " ms");
